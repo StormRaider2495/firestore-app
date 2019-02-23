0742b2d8536b8a7d4afff55b872bb964931c1754
LEARNINGMATEKOL\arunit.mazumdar <arunit.mazumdar@learningmate.com>
2019-02-23 13:36:41 +0530

    firestore-app setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ionic 4 and Angular 6 Tutorial: Firebase Realtime CRUD Mobile App
+
+This source code is part of 
+[Building a CRUD Ionic application with Firestore](https://javebratt.com/crud-ionic-firestore/) tutorial.
+
+To run locally:
+
+* Clone this repo
+* Run `npm install`
+* Run `ionic serve`
---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBxqYcusxkFqgGdGTw6QCyyDqUeg2A8nYE',
+    authDomain: 'bulletinboard-e03c0.firebaseapp.com',
+    databaseURL: 'https://bulletinboard-e03c0.firebaseio.com',
+    projectId: 'bulletinboard-e03c0',
+    storageBucket: 'bulletinboard-e03c0.appspot.com',
+    messagingSenderId: '1044295125404'
+};
