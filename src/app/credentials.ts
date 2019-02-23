77bed798fd23d9e2a4bcecce9f58f5292cb61e69
LEARNINGMATEKOL\arunit.mazumdar <arunit.mazumdar@learningmate.com>
2019-02-23 16:41:22 +0530

    CRUD functionalities implemented for the app

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: 'AIzaSyBxqYcusxkFqgGdGTw6QCyyDqUeg2A8nYE',
-    authDomain: 'bulletinboard-e03c0.firebaseapp.com',
-    databaseURL: 'https://bulletinboard-e03c0.firebaseio.com',
-    projectId: 'bulletinboard-e03c0',
-    storageBucket: 'bulletinboard-e03c0.appspot.com',
-    messagingSenderId: '1044295125404'
+    apiKey: 'AIzaSyCHNsppVPJ6zuV-ie3BxLAPGFiKzY0LtpQ',
+    authDomain: 'firestore-app-b94f6.firebaseapp.com',
+    databaseURL: 'https://firestore-app-b94f6.firebaseio.com',
+    projectId: 'firestore-app-b94f6',
+    storageBucket: 'firestore-app-b94f6.appspot.com',
+    messagingSenderId: '949245509058'
 };
--- src/app/pages/create/create.page.ts
+++ src/app/pages/create/create.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LoadingController, AlertController } from '@ionic/angular';
+import { FirestoreService } from '../../services/data/firestore.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePage implements OnInit {
 
-  constructor() { }
+  public createSongForm: FormGroup;
+
+  constructor(
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public firestoreService: FirestoreService,
+    private router: Router,
+    formBuilder: FormBuilder
+  ) {
+    this.createSongForm = formBuilder.group({
+      albumName: ['', Validators.required],
+      artistName: ['', Validators.required],
+      songDescription: ['', Validators.required],
+      songName: ['', Validators.required],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  async createSong() {
+    const loading = await this.loadingCtrl.create();
+
+    const albumName = this.createSongForm.value.albumName;
+    const artistName = this.createSongForm.value.artistName;
+    const songDescription = this.createSongForm.value.songDescription;
+    const songName = this.createSongForm.value.songName;
+
+    this.firestoreService
+    .createSong(albumName, artistName, songDescription, songName)
+    .then(
+      () => {
+        loading.dismiss().then(() => {
+          this.router.navigateByUrl('');
+        });
+      },
+      error => {
+        console.error(error);
+      }
+    );
+
+    return await loading.present();
   }
 
 }
